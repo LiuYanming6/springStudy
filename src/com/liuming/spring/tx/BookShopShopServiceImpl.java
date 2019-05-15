819d6d434312ff7240ca17ed3b8decec1ba218ac
liu <a@a.com>
2019-05-15 18:54:58 +0800

    day 7:事务注解

--- src/com/liuming/spring/tx/BookShopShopServiceImpl.java
+++ src/com/liuming/spring/tx/BookShopShopServiceImpl.java
@@ -2,17 +2,21 @@ package com.liuming.spring.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.
 
 /**
  * @Author: 刘艳明
  * @Date: 19-5-15 下午5:57
  */
 @Service
+import org.springframework.transaction.annotation.Transactional;
+
 public class BookShopShopServiceImpl implements BookShopService {
 
     @Autowired
     private BookShopDao bookShopDao;
 
+    @Transactional
     @Override
     public void purchase(String username, String isbn) {
         // 1.获取书的单价
